d3d2d88d6260b8771606ec8c72e6c35e49b3a739
贤心 <3277200+sentsim@users.noreply.github.com>
2024-07-31 16:35:20 +0800

    release: v0.3.0

--- dist/index.cjs
+++ dist/index.cjs
@@ -14,13 +14,19 @@ function eventsourceParser(chunk, callback, options = {}) {
   } = options;
   const buffer = (instance.buffer || '') + chunk;
   const lines = buffer.split('\n');
+  const onMessage = message => callback?.(message);
 
   // SSE 规范要求每个事件使用空行即 \n\n 进行分割
   // 移除最后一行，若事件完整则为空行，若事件不完整则留作下次处理
   instance.buffer = lines.pop();
 
+  // 若数据为空则直接返回
+  if (lines.length === 0) {
+    return onMessage(message);
+  }
+
   // 解析原始数据
-  lines.forEach((item, index) => {
+  lines.forEach(item => {
     // 空行表示一个完整事件的结束
     if (item.trim() === '') {
       // 是否自动解析 JSON
@@ -29,15 +35,15 @@ function eventsourceParser(chunk, callback, options = {}) {
           message.data = JSON.parse(message.data);
         } catch (e) {}
       }
-      callback?.(message, index); // 返回数据
+      onMessage(message); // 返回数据
       message = {}; // 重置事件返回的数据
     } else {
       const fieleLength = item.indexOf(':');
-      const field = item.substring(0, fieleLength);
-      const value = item.substring(fieleLength + 1);
+      const field = item.substring(0, fieleLength).trim();
+      const value = item.substring(fieleLength + 1).trim();
 
       // 处理数据
-      if (field.trim()) {
+      if (field) {
         switch (field) {
           case 'data':
             message.data = value;
@@ -108,9 +114,9 @@ class FetchEventSource {
           parseJson
         }); // 解析 SSE 数据格式
 
-        readStream();
+        await readStream();
       }
-      readStream();
+      await readStream();
     } catch (error) {
       this.abort();
       delete this.buffer;
